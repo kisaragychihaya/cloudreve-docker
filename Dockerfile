b71380c6aeae036d3dfa74b04e1f00be78e9561f
kisaragychihaya <hetongtech@hotmail.com>
2024-04-14 21:26:35 +0800

    Use Random SALT

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,16 @@
-FROM  --platform=arm64 debian:stable-slim
+ARG ARCH=amd64
+FROM  --platform=${ARCH} debian:stable-slim
+ARG ARCH=amd64
 LABEL maintainer="kisaragychihaya"
+RUN sed -i 's/deb.debian.org/mirrors.ustc.edu.cn/g' /etc/apt/sources.list.d/debian.sources 
+RUN apt-get update && apt-get -y install aria2 && apt clean
 WORKDIR /core
 COPY ./src ./
-RUN mkdir ./uploads ./db ./log && touch /core/etc/aria2/aria2.session 
-RUN apt-get -y install aria2
-ADD cloudreve_3.8.3_linux_arm64.tar.gz /core/
-ADD https://raw.githubusercontent.com/kisaragychihaya/cloudreve-docker/master/src/aria2/conf/dht.dat /core/etc/aria2/dht.dat
+RUN mkdir ./uploads ./db ./log && touch /core/etc/aria2/aria2.session
+ADD https://github.com/cloudreve/Cloudreve/releases/download/3.8.3/cloudreve_3.8.3_${ARCH}_amd64.tar.gz /core/
+ADD https://github.com/WumaCoder/cloudreve-docker/raw/master/src/aria2/conf/dht.dat /core/etc/aria2/dht.dat
+RUN chmod 755 /core/start.sh
+VOLUME [ "/core/uploads","/core/db","/core/log" ]
 CMD /bin/bash /core/start.sh
 EXPOSE 5212
 
--- src/start.sh
+++ src/start.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 if [ ! -f "/core/etc/conf.ini" ];then
-echo "文件不存在"
-echo "[System]" > /core/etc/conf.ini
-echo "Mode = master" >> /core/etc/conf.ini
-echo "Listen = :5212" >> /core/etc/conf.ini
-echo "Debug = false" >> /core/etc/conf.ini
-echo "[Database]" >> /core/etc/conf.ini
-echo "DBFile = /core/db/cloudreve.db" >> /core/etc/conf.ini
+	echo "fnf"
+	echo "[System]" > /core/etc/conf.ini
+	echo "Mode = master" >> /core/etc/conf.ini
+	echo "Listen = :5212" >> /core/etc/conf.ini
+	echo "Debug = false" >> /core/etc/conf.ini
+	echo "[Database]" >> /core/etc/conf.ini
+	echo "DBFile = /core/db/cloudreve.db" >> /core/etc/conf.ini
 fi
+rand_sec=$(cat /dev/urandom | head -n 10 | md5sum | cut -c 1-16)
+sed -i 's/Your_CR_SEC/'${rand_sec}'/g' /core/etc/conf.ini
+rand_sec=$(cat /dev/urandom | head -n 10 | md5sum | cut -c 1-16)
+sed -i 's/Sth_Deep_Sth_Dark_Out_of_Fantasty/'${rand_sec}'/g' /core/etc/conf.ini
 /bin/chmod +x /core/cloudreve
-nohup /bin/aria2c --conf /core/aria2/conf/aria2.conf > /core/log/aria2.log 2>&1 &
+aria2c --conf /core/etc/aria2/conf.ini -D
 /core/cloudreve -c /core/etc/conf.ini > /core/log/cloudreve.log
